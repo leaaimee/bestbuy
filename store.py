6803109a18338b06776e60c8947d0f0ff3dba702
Lea <leaaimee2010@gmail.com>
2025-01-12 17:41:29 +0100

    part 2 of the bestbuy project

--- store.py
+++ store.py
@@ -1,3 +1,39 @@
+from products import Product
 
+class Store:
 
+    def __init__(self, products):
+        self.products = products
 
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+    def get_total_quantity(self) -> int:
+        return sum(product.get_quantity() for product in self.products)
+
+    def get_all_products(self) -> list:
+        return [product for product in self.products if product.is_active()]
+
+    def order(self, shopping_list) -> float:
+        total_price = 0
+        for product, quantity in shopping_list:
+            total_price += product.buy(quantity)
+        return total_price
+
+def main():
+
+    product_list = [Product("MacBook Air M2", price=1450, quantity=100),
+                    Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    Product("Google Pixel 7", price=500, quantity=250),
+                    ]
+
+    store = Store(product_list)
+    products = store.get_all_products()
+    print(store.get_total_quantity())
+    print(store.order([(products[0], 1), (products[1], 2)]))
+
+if __name__ == "__main__":
+    main()
